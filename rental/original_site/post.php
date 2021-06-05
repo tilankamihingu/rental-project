75490682d47232cf61673ad35f8a36af74cc4d69
lahiru Pramod <lahirupramod41@gmail.com>
2021-06-05 17:18:10 +0530

    home page backend

--- rental/original_site/post.php
+++ rental/original_site/post.php
@@ -148,7 +148,7 @@ move_uploaded_file($temp_name, $upload_to . $file_name);
                         <h2>Select Category</h2>
                         <select id="inputState"  name="cate"<?php echo 'value="' .$cate . '"';?>>
                             <option>Cars</option>
-                            <option>Cars</option>
+                            <option>clothes</option>
                             <option>Cars</option>
                             <option>Cars</option>
                         </select>
